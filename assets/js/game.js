4066d96aaf335827b85763ab44dce1122c6fca23
Steven Cruz <cruz801@hotmail.com>
2021-11-05 20:47:27 -0600

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,6 +13,13 @@ console.log(enemyNames[1]);
 console.log(enemyNames[2]);
 console.log(enemyNames.length);
 
+// function to generate a random numeric value
+var randomNumber = function(min,max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+}
+
 var fight = function (enemyName) {
 
    
@@ -32,16 +39,17 @@ var fight = function (enemyName) {
     if (confirmSkip) {
         window.alert(playerName + " has decided to skip this fight. Goodbye!");
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
       }
     }
   
-  // if player chooses to fight, then fight
-//   if (promptFight === "fight" || promptFight === "FIGHT") {
+  
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    // generate random damage value based on player's attack power
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName +
         " attacked " +
@@ -62,7 +70,8 @@ var fight = function (enemyName) {
     }
 
     // remove player's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName +
         " attacked " +
@@ -101,7 +110,7 @@ if (playerHealth > 0) {
     var pickedEnemyName = enemyNames[i];
 
     //reset enemyHealth before starting new fight
-    enemyHealth = 50;
+    enemyHealth = randomNumber(40,60);
 
     // use debugger to pause script from running and what's going on at the moment in the code
    // debugger;
